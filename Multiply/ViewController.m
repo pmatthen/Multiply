f7a2c48d1810c300bdad29344e587167242d320e
Gule Sheikh <gulesh@gmail.com>
2014-01-07 16:44:23 -0600

    Poulose and Gule MVP Complete

--- Multiply/ViewController.m
+++ Multiply/ViewController.m
@@ -9,7 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    
+    __weak IBOutlet UITextField *myNumber;
+    __weak IBOutlet UILabel *myMultiplier;
+    __weak IBOutlet UILabel *myAnswer;
+    
+}
 @end
 
 @implementation ViewController
@@ -18,6 +24,19 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)onCalculateButtonPressed:(id)sender
+{
+    int multiplier = myMultiplier.text.intValue;
+   // NSLog(@"multiplier value is %i", multiplier);
+    int number = myNumber.text.intValue;
+    int total = number * multiplier;
+    
+  //  myAnswer.text = [NSString st]
+  //  [myAnswer setText:@"%i", total];
+    myAnswer.text = [NSString stringWithFormat:@"%i", total];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
