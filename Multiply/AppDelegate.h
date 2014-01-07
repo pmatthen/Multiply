405e1aa4d2a84235b0014a03be53b57829adf01f
Gule Sheikh <gulesh@gmail.com>
2014-01-07 15:49:13 -0600

    Initial Commit

---
+++ Multiply/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Multiply
+//
+//  Created by gule on 1/7/2014.
+//  Copyright (c) 2014 gule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Multiply/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Multiply
+//
+//  Created by gule on 1/7/2014.
+//  Copyright (c) 2014 gule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
